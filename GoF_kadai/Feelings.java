0a60da443ab6ed9da2de2172b21d75c877a153b4
Kota Nakachi <s15007@std.it-college.ac.jp>
2016-06-29 22:54:00 +0900

    Adapterパターン（継承を使った場合）

---
+++ GoF_kadai/Feelings.java
@@ -0,0 +1,46 @@
+public class Feelings {
+	public static void main(String[] args) {
+		CurrentFeeling now = new CurrentFeeling();
+		now.angry();
+		now.happy();
+		now.sad();
+		now.stupid();
+	}
+}
+
+interface MyFeelings {
+	public abstract void angry();
+	public abstract void happy();
+	public abstract void sad();
+	public abstract void stupid();
+}
+
+class Me {
+	public void beAngy() {
+		System.out.println("\n(∴｀┏ω┓´)/ｺﾗｧｰ!!");
+	}
+	public void beHappy() {
+		System.out.println("\nヽ(´▽｀)/へへっ");
+	}
+	public void beSad() {
+		System.out.println("\n(ＴｍＴ)ｳｩｩ･･･");
+	}
+	public void beStupid() {
+		System.out.println("\n└(ﾟ∀ﾟ└)ﾊｯｽﾙ!ﾊｯｽﾙ!!(┘ﾟ∀ﾟ)┘ ﾊｯｽﾙ!ﾊｯｽﾙ!!\n");
+	}
+}
+// 継承を使ったAdapterパターン↓↓↓
+class CurrentFeeling extends Me implements MyFeelings {
+	public void angry() {
+		beAngy();
+	}
+	public void happy() {
+		beHappy();
+	}
+	public void sad() {
+		beSad();
+	}
+	public void stupid() {
+		beStupid();
+	}
+}
